3209dacb82ab970a7fa10e788af80a9e25108153
Jun Lee <jun@main-10-230-23-59.tamulink.tamu.edu>
2023-08-29 11:09:34 -0500

    frontend first commit

--- util/modals/searchModal.js
+++ util/modals/searchModal.js
@@ -1,7 +1,7 @@
 import "./searchModal.css";
 import Link from "next/link";
 import Image from "next/image";
-// for commit
+// for commit frontend
 export default function SearchModal({
   closeModal,
   effectModal,
